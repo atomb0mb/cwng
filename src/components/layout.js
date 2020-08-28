5fea5554a6705e0883926cb48adfa5d935cc5884
Chee Ng <cwng@ncsu.edu>
2020-08-28 13:16:42 -0400

    Updated node retrieval

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ const ListLink = props => (
 // Navigation display
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }} media="print">
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>CWNG</h3>
